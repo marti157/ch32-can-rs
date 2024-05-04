b508d81bcc2539f6399ccd537428861991b070bd
marti157 <31134295+marti157@users.noreply.github.com>
2024-05-04 16:25:13 +0200

    Convert Can module to struct

--- scenarios/silent_loopback/main.rs
+++ scenarios/silent_loopback/main.rs
@@ -1,9 +1,10 @@
 #![no_std]
 #![no_main]
 
+use ch32_can_rs::{Can, CanFifo, CanMode};
 use hal::println;
 use qingke::riscv;
-use {ch32_can_rs as can, ch32_hal as hal, panic_halt as _};
+use {ch32_hal as hal, panic_halt as _};
 
 #[qingke_rt::entry]
 fn main() -> ! {
@@ -12,9 +13,11 @@ fn main() -> ! {
     config.rcc = hal::rcc::Config::SYSCLK_FREQ_96MHZ_HSI;
     hal::init(config);
 
+    let can = Can::new(CanFifo::FIFO1);
+
     println!("Starting init CAN silent loopback mode.");
 
-    match can::initialize(can::CanMode::SILENT_LOOPBACK) {
+    match can.init_mode(CanMode::SILENT_LOOPBACK) {
         Ok(_) => println!("Initialized CAN in silent loopback mode."),
         Err(msg) => {
             println!("Error initializing CAN: {msg}");
@@ -29,11 +32,11 @@ fn main() -> ! {
     loop {
         riscv::asm::delay(50000000);
 
-        can::send_message_mbox0(msg, 0x317);
+        can.send_message_mbox0(msg, 0x317);
         println!("Sent CAN message.");
 
         println!("Read CAN message:");
-        match can::receive_message_no_checks(can::CanFifo::FIFO1) {
+        match can.receive_message_no_checks() {
             None => println!("No message."),
             Some(recv_msg) => println!("0x{:x}", recv_msg),
         }
--- src/lib.rs
+++ src/lib.rs
@@ -50,101 +50,117 @@ const CAN_TQBS1: u8 = 0b000;
 const CAN_TQBS2: u8 = 0b000;
 const CAN_PRESCALER: u16 = 12;
 
-fn init_default_filter() {
-    let filter_num = 1;
-
-    CAN.fctlr().modify(|w| w.set_finit(true)); // Enable filter init mode
-    CAN.fwr().modify(|w| w.set_fact(filter_num, true)); // Activate filter 1 in filter bank
-    CAN.fscfgr().modify(|w| w.set_fsc(filter_num, true)); // Set register of filter 1 to single 32-bit
-    CAN.fr(filter_num * 2 + 0)
-        .write_value(pac::can::regs::Fr(0b110)); // Masking bits in identifier: 110
-    CAN.fr(filter_num * 2 + 1)
-        .write_value(pac::can::regs::Fr(0x0000)); // Not masking any bits
-    CAN.fmcfgr().modify(|w| w.set_fbm(filter_num, false)); // Set filter 1 to mask bit mode (0)
-    CAN.fafifor().modify(|w| w.set_ffa(filter_num, true)); // Associate FIFO1 to filter 1
-    CAN.fwr().modify(|w| w.set_fact(filter_num, true)); // Activate filter 1
-    CAN.fctlr().modify(|w| w.set_finit(false)); // Exit filter init mode
+pub struct Can {
+    fifo: CanFifo,
 }
 
-pub fn initialize(mode: CanMode) -> Result<(), &'static str> {
-    RCC.apb1pcenr().modify(|w| w.set_can1en(true)); // Enable CAN1 peripheral
+impl Can {
+    pub fn new(fifo: CanFifo) -> Self {
+        Self::enable();
 
-    CAN.ctlr().modify(|w| {
-        w.set_sleep(false); // Wake up
-        w.set_inrq(true); // Request enter init mode
-    });
-
-    let mut wait_ack: u32 = 0;
-    // Wait until CAN is in init mode
-    while !CAN.statr().read().inak() && wait_ack < CAN_INAK_TIMEOUT {
-        wait_ack += 1;
+        Self { fifo }
     }
 
-    if !CAN.statr().read().inak() {
-        return Err("CAN peripheral did not enter initialization mode");
+    fn enable() {
+        RCC.apb1pcenr().modify(|w| w.set_can1en(true)); // Enable CAN1 peripheral
     }
 
-    // CAN baud rate is: CANbps=PCLK1/((TQBS1+TQBS2+3)*(PRESCALER+1))
-    CAN.btimr().modify(|w| {
-        w.set_brp(CAN_PRESCALER - 1); // Set CAN1 time quantum length
-        w.set_ts1(CAN_TQBS1); // Set CAN1 time quantum in bit segment 1
-        w.set_ts2(CAN_TQBS2); // Set CAN1 time quantum in bit segment 2
-        w.set_sjw(CAN_SJW); // Set CAN1 resync jump width
-        w.set_lbkm(mode.lbkm); // Set silent mode bit from mode
-        w.set_silm(mode.silm); // Set loopback mode bit from mode
-    });
+    pub fn init_mode(&self, mode: CanMode) -> Result<(), &'static str> {
+        RCC.apb1pcenr().modify(|w| w.set_can1en(true)); // Enable CAN1 peripheral
 
-    CAN.ctlr().modify(|w| w.set_inrq(false)); // Request exit init mode
+        CAN.ctlr().modify(|w| {
+            w.set_sleep(false); // Wake up
+            w.set_inrq(true); // Request enter init mode
+        });
 
-    wait_ack = 0;
-    // Wait until CAN is no longer in init mode
-    while CAN.statr().read().inak() && wait_ack < CAN_INAK_TIMEOUT {
-        wait_ack += 1;
+        let mut wait_ack: u32 = 0;
+        // Wait until CAN is in init mode
+        while !CAN.statr().read().inak() && wait_ack < CAN_INAK_TIMEOUT {
+            wait_ack += 1;
+        }
+
+        if !CAN.statr().read().inak() {
+            return Err("CAN peripheral did not enter initialization mode");
+        }
+
+        // CAN baud rate is: CANbps=PCLK1/((TQBS1+TQBS2+3)*(PRESCALER+1))
+        CAN.btimr().modify(|w| {
+            w.set_brp(CAN_PRESCALER - 1); // Set CAN1 time quantum length
+            w.set_ts1(CAN_TQBS1); // Set CAN1 time quantum in bit segment 1
+            w.set_ts2(CAN_TQBS2); // Set CAN1 time quantum in bit segment 2
+            w.set_sjw(CAN_SJW); // Set CAN1 resync jump width
+            w.set_lbkm(mode.lbkm); // Set silent mode bit from mode
+            w.set_silm(mode.silm); // Set loopback mode bit from mode
+        });
+
+        CAN.ctlr().modify(|w| w.set_inrq(false)); // Request exit init mode
+
+        wait_ack = 0;
+        // Wait until CAN is no longer in init mode
+        while CAN.statr().read().inak() && wait_ack < CAN_INAK_TIMEOUT {
+            wait_ack += 1;
+        }
+
+        if CAN.statr().read().inak() {
+            return Err("CAN peripheral did not exit initialization mode");
+        }
+
+        self.init_default_filter();
+
+        Ok(())
     }
 
-    if CAN.statr().read().inak() {
-        return Err("CAN peripheral did not exit initialization mode");
+    fn init_default_filter(&self) {
+        let filter_num = 1;
+
+        CAN.fctlr().modify(|w| w.set_finit(true)); // Enable filter init mode
+        CAN.fwr().modify(|w| w.set_fact(filter_num, true)); // Activate filter 1 in filter bank
+        CAN.fscfgr().modify(|w| w.set_fsc(filter_num, true)); // Set register of filter 1 to single 32-bit
+        CAN.fr(filter_num * 2 + 0)
+            .write_value(pac::can::regs::Fr(0b110)); // Masking bits in identifier: 110
+        CAN.fr(filter_num * 2 + 1)
+            .write_value(pac::can::regs::Fr(0x0000)); // Not masking any bits
+        CAN.fmcfgr().modify(|w| w.set_fbm(filter_num, false)); // Set filter 1 to mask bit mode (0)
+        CAN.fafifor().modify(|w| w.set_ffa(filter_num, true)); // Associate FIFO1 to filter 1
+        CAN.fwr().modify(|w| w.set_fact(filter_num, true)); // Activate filter 1
+        CAN.fctlr().modify(|w| w.set_finit(false)); // Exit filter init mode
     }
 
-    init_default_filter();
-
-    Ok(())
-}
-
-pub fn send_message_mbox0(message: u64, stid: u16) {
-    let mailbox_num: usize = 0;
-    CAN.txmdtr(mailbox_num).modify(|w| w.set_dlc(8)); // Set message length in bytes
-    CAN.txmdhr(mailbox_num)
-        .write_value(pac::can::regs::Txmdhr((message >> 32) as u32));
-    CAN.txmdlr(mailbox_num)
-        .write_value(pac::can::regs::Txmdlr((message & 0xFFFFFFFF) as u32));
-    CAN.txmir(mailbox_num)
-        .write_value(pac::can::regs::Txmir(0x0)); // Clear CAN1 TXMIR register
-    CAN.txmir(mailbox_num).modify(|w| {
-        w.set_stid(stid); // Using CAN Standard ID for message
-        w.set_txrq(true); // Initiate mailbox transfer request
-    });
-}
-
-pub fn receive_message_no_checks(fifo: CanFifo) -> Option<u64> {
-    let num_pending_messages = match fifo {
-        CanFifo::FIFO0 => CAN.rfifo0().read().fmp0(),
-        CanFifo::FIFO1 => CAN.rfifo1().read().fmp1(),
-    };
-
-    if num_pending_messages == 0 {
-        return None;
+    pub fn send_message_mbox0(&self, message: u64, stid: u16) {
+        let mailbox_num: usize = 0;
+        CAN.txmdtr(mailbox_num).modify(|w| w.set_dlc(8)); // Set message length in bytes
+        CAN.txmdhr(mailbox_num)
+            .write_value(pac::can::regs::Txmdhr((message >> 32) as u32));
+        CAN.txmdlr(mailbox_num)
+            .write_value(pac::can::regs::Txmdlr((message & 0xFFFFFFFF) as u32));
+        CAN.txmir(mailbox_num)
+            .write_value(pac::can::regs::Txmir(0x0)); // Clear CAN1 TXMIR register
+        CAN.txmir(mailbox_num).modify(|w| {
+            w.set_stid(stid); // Using CAN Standard ID for message
+            w.set_txrq(true); // Initiate mailbox transfer request
+        });
     }
 
-    // No message length checks
-    let received_message: u64 =
-        ((CAN.rxmdhr(fifo.val()).read().0 as u64) << 32) | CAN.rxmdlr(fifo.val()).read().0 as u64;
+    pub fn receive_message_no_checks(&self) -> Option<u64> {
+        let num_pending_messages = match self.fifo {
+            CanFifo::FIFO0 => CAN.rfifo0().read().fmp0(),
+            CanFifo::FIFO1 => CAN.rfifo1().read().fmp1(),
+        };
 
-    // Release FIFO
-    match fifo {
-        CanFifo::FIFO0 => CAN.rfifo0().modify(|w| w.set_rfom0(true)),
-        CanFifo::FIFO1 => CAN.rfifo1().modify(|w| w.set_rfom1(true)),
+        if num_pending_messages == 0 {
+            return None;
+        }
+
+        // No message length checks
+        let received_message: u64 = ((CAN.rxmdhr(self.fifo.val()).read().0 as u64) << 32)
+            | CAN.rxmdlr(self.fifo.val()).read().0 as u64;
+
+        // Release FIFO
+        match self.fifo {
+            CanFifo::FIFO0 => CAN.rfifo0().modify(|w| w.set_rfom0(true)),
+            CanFifo::FIFO1 => CAN.rfifo1().modify(|w| w.set_rfom1(true)),
+        }
+
+        Some(received_message)
     }
-
-    Some(received_message)
 }
